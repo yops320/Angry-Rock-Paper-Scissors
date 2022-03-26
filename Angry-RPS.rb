2ed48c8417e71c4fabc5450b4ce7a94f817fead4
Liam D <94531542+yops320@users.noreply.github.com>
2022-03-26 10:42:38 +1030

    Update Angry-RPS.rb
    Added featurs such as sleep(x) and a slow print

--- Angry-RPS.rb
+++ Angry-RPS.rb
@@ -1,37 +1,55 @@
+#Hello ask for name
 puts "Greetings. What is your name?"
 name = gets.chomp
+#chooses name for user 
 insult = ["Idiot", "Dumbass", "Simpleton", "Stupid"].sample
+#make like cpu is thinking
+"#{name}...".each_char {|c| putc c ; sleep 0.05; $stdout.flush }
+sleep(2)
+#make like cpu is thinking
+"That doesn't suit you.".each_char {|c| putc c ; sleep 0.05; $stdout.flush }
+#pause for effect
+sleep(2) 
+#acknowledges users new name
+puts "\nI'd rather call you #{insult}." 
+#pause for effect
+sleep(2)
 #What the game is/does the user want to play
-puts "#{name}. I'd rather call you #{insult}. Would you like to play rock, paper, scissors #{insult}? [y/n]"
+puts "Would you like to play rock, paper, scissors, #{insult}? [y/n]"
 # yes/no/invalid
 answer=gets.chomp.downcase
 if answer == 'n'
-puts "That makes me angry"
-    elsif  answer == 'y'
-    puts "Me too #{insult}!"
-#scores
+#cpu gets upset
+"That makes me angry \n".each_char {|c| putc c ; sleep 0.05; $stdout.flush }
+sleep(0.5)
+"I just wanted to play a game \n".each_char {|c| putc c ; sleep 0.05; $stdout.flush }
+sleep(0.5)
+"I won't forget this".each_char {|c| putc c ; sleep 0.05; $stdout.flush }
+elsif  answer == 'y'
+puts "Me too #{insult}! First to 3."    
+#startingscore
 computerScore = 0
 userScore = 0
 #end of game (could go to any number)
-until computerScore == 1 || userScore == 1
-    puts "Rock, paper or scissors?"
-    user = gets.chomp.downcase
-    computer = ["rock", "paper", "scissors"].sample
-    #user wins
-    if (user == "rock" && computer == "scissors") || (user == "scissors" && computer == "paper") || (user == "paper" && computer == "rock")
-        puts "You won #{insult}!"
-        userScore += 1
-    #draws
-    elsif (user == "rock" && computer == "rock") || (user == "paper" && computer == "paper") || (user == "scissors" && computer == "scissors")
-        puts "Draw! Let's try again #{insult}!"
-    #computer wins
-    else computerScore += 1
-        puts "You lose #{insult}."   
-    end
-    #Choices
-    puts "#{insult} chose: #{user}"
-    puts "Computer chose: #{computer}"
+until computerScore == 3 || userScore == 3
+puts "Rock, paper or scissors?"
+user = gets.chomp.downcase
+computer = ["rock", "paper", "scissors"].sample
+#user winner
+if (user == "rock" && computer == "scissors") || (user == "scissors" && computer == "paper") || (user == "paper" && computer == "rock")
+"You won #{insult}. You must be cheating!\n".each_char {|c| putc c ; sleep 0.05; $stdout.flush } 
+userScore += 1
+#tie
+elsif (user == "rock" && computer == "rock") || (user == "paper" && computer == "paper") || (user == "scissors" && computer == "scissors")
+puts "Draw! Let's try again #{insult}!"
+#cpu winner
+else computerScore += 1
+puts "You lose #{insult}!"   
 end
-    #Winner
-    puts userScore > computerScore ? ("#{insult} WINS!"):("HAHAHA I WIN!")
+#who chose what
+puts "#{insult} chose: #{user}"
+puts "Computer chose: #{computer}"
+end
+#end of game
+puts userScore > computerScore ? ("#{insult} is the winner\n" "You probably cheated..."):("HAHAHA" "\nI AM THE WINNNER!")
 end
